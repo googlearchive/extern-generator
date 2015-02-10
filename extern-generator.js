7d1e6ecf780eb8fbf4c39c1d83143b6b0df9a8b8
AJ Ortega <ajo@renitservices.com>
2015-02-10 15:32:45 -0800

    Bring upstream changes in.
    Includes changes from robliao@google.com.

--- extern-generator.js
+++ extern-generator.js
@@ -8,34 +8,65 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-(function (){
+var argv = require('optimist')
+    .alias('webcomponent', 'w')
+    .alias('externs', 'x')
+    .alias('events', 'e')
+    .string(['w', 'x', 'e'])
+    .demand(['w', 'x', 'e'])
+    .usage('$0 -w [input] -x [externs_file] -e [event_enum_file]')
+    .argv;
+
+var input, externs, events;
+
+input = argv.w;
+externs = argv.x;
+events = argv.e;
+
+process.stderr.write(input + '\n');
+process.stderr.write(externs + '\n');
+process.stderr.write(events + '\n');
+
+(function() {
   util = require('util');
   fs = require('fs');
-  ContextFreeParser = require('../context-free-parser');
+  ContextFreeParser = require('context-free-parser');
 
+  function readSnippet(snippetName) {
+    return fs.readFileSync(__dirname + '/' + snippetName, 'UTF-8');
+  }
   // Static text
-  headerTemplate = fs.readFileSync('header-snippet.txt', {encoding: 'UTF-8'});
+  headerTemplate = readSnippet('header-snippet.txt');
 
   function headerSnippet(cls) {
-    camelCaseParent = className(cls.extends);
+    // Transitional fix for Polymer 0.5.4
+    // Polymer 0.5.4's context-free-parser modifies the extends field to an
+    // array of objects from a simple string.
+    // TODO(robliao): Remove the cls.extends portion once the upgrade is
+    //     complete.
+    camelCaseParent = className(
+        Array.isArray(cls.extends) ? cls.extends[0].name : cls.extends);
     camelCaseClass = className(cls.name);
-    return util.format(headerTemplate, cls.name, camelCaseParent, camelCaseClass);
+    return util.format(headerTemplate,
+                       cls.name,
+                       camelCaseParent,
+                       camelCaseClass);
   }
 
   function camelCase(name) {
-    return name.split('-').map(function(name){
+    return name.split('-').map(function(name) {
       return name.toLowerCase().charAt(0).toUpperCase() + name.slice(1);
-    }).join("")
+    }).join('');
   }
 
   function className(name) {
     if (name == undefined) {
-      return "PolymerElement";
+      return 'PolymerElement';
     }
-    return camelCase(name)+"Element";
+    return camelCase(name) + 'Element';
   }
 
-  attributeTemplate = fs.readFileSync('attribute-snippet.txt', {encoding: 'UTF-8'});
+  attributeTemplate = readSnippet('attribute-snippet.txt');
   function attributeSnippet(cls, attr) {
     return util.format(attributeTemplate,
                        commentify(attr.description),
@@ -44,64 +75,67 @@
                        attr.name);
   }
 
+  function methodSnippet(cls, meth) {
+    return util.format(attributeTemplate,
+                       commentify(meth.description),
+                       'function()',
+                       className(cls.name),
+                       meth.name);
+  }
+
   function commentify(description) {
     // Splitting on newlines gives us one more than we need for nicely
     // formatted comments.
-    splitlines = description.split("\n").slice(0, -1);
-    return splitlines.map(function(line){
-      return " * " + line
-    }).join("\n");
+    splitlines = description.split('\n').slice(0, -1);
+    return splitlines.map(function(line) {
+      return ' * ' + line;
+    }).join('\n');
   }
 
-  eventTemplate = fs.readFileSync('event-snippet.txt', {encoding: 'UTF-8'});
+  eventTemplate = readSnippet('event-snippet.txt');
   function eventSnippet(cls) {
     if (cls.events === undefined) {
-      return ''
+      return '';
     }
-    eventType = camelCase(cls.name)+'EventType';
-    eventDefs = cls.events.map(function(evt){
-      snippet = evt.description.trim().split('\n').map(function(line){
-        return '  // ' + line
+    eventType = camelCase(cls.name) + 'EventType';
+    eventDefs = cls.events.map(function(evt) {
+      snippet = evt.description.trim().split('\n').map(function(line) {
+        return '  // ' + line;
       }).join('\n');
-      snippet += '\n  ' + evt.name.toUpperCase().replace('-', '_') + ": '" + evt.name + "'";
+      snippet += '\n  ' + evt.name.toUpperCase().replace(/-/g, '_') + ": '" +
+          evt.name + "'";
       return snippet;
     }).join(',\n');
-    return util.format(eventTemplate, eventType, cls.name, eventType, eventDefs)
+    return util.format(
+        eventTemplate, eventType, cls.name, eventType, eventDefs);
   }
 
-  input = process.argv[2];
-  externs = process.argv[3];
-  events = process.argv[4];
-
-  [input, externs, events].forEach(function(file){
-    if (file == undefined) {
-      console.log("usage:\nnode extern-generator.js [input] [externs_file] [event_enum_file]");
-      process.exit(1);
-    }
-  });
-
   // Get input buffer
-  inputText = fs.readFileSync(input, {encoding: 'UTF-8'});
+  inputText = fs.readFileSync(input, 'UTF-8');
   // Parse input
   classes = ContextFreeParser.parse(inputText);
   // Build output string
   outputBuffer = '';
 
   eventOutputBuffer = '';
-  classes.forEach(function(cls){
-    outputBuffer += headerSnippet(cls);
-    if (cls.attributes) {
-      cls.attributes.forEach(function(attr){
-        outputBuffer += attributeSnippet(cls, attr);
-      });
+  classes.forEach(function(cls) {
+    if (cls.name.toLowerCase() !== 'entity') {
+      outputBuffer += headerSnippet(cls);
+      if (cls.attributes) {
+        cls.attributes.forEach(function(attr) {
+          outputBuffer += attributeSnippet(cls, attr);
+        });
+      }
+      if (cls.methods) {
+        cls.methods.forEach(function(meth) {
+          outputBuffer += methodSnippet(cls, meth);
+        });
+      }
+      eventOutputBuffer += eventSnippet(cls);
     }
-    eventOutputBuffer += eventSnippet(cls)
   });
 
   // Write output buffer
-  //console.log(outputBuffer);
   fs.writeFileSync(externs, outputBuffer);
-  if (eventOutputBuffer.length > 0) {
-    fs.writeFileSync(events, eventOutputBuffer);
-  }
+  fs.writeFileSync(events, eventOutputBuffer);
 }());
